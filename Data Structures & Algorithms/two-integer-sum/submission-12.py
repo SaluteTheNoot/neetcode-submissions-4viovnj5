d45bc0fe732c6756c605ee1884066f996d8e3b2d
Yousuf Khalid <93394402+SaluteTheNoot@users.noreply.github.com>
2026-04-07 17:37:28 -0500

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #given array of ints nums and int target
+        #return index I and J where nums[I] + nums[J] == target and I != J
+        #every input has a solution, so you dont need to worry about that
+
+        #you can easily use two pointers for O(n^2)
+        
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    if i < j:
+                        return [i,j] #smallest first so deal with that later
+                    else:
+                        return [j,i]
+
+        
